477feaa6f338df08900e03a4e03831b417aa7c6d
SilentCC <17816853696@163.com>
2018-07-04 22:21:48 +0800

    ported LocaleSymtaxException

--- src/ICU4N/ICU4N/Impl/Locale/LocaleSyntaxException.cs
+++ src/ICU4N/ICU4N/Impl/Locale/LocaleSyntaxException.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace ICU4N.Impl.Locale
 {
-    class LocaleSyntaxException
+    class LocaleSyntaxException : Exception
     {
+        private static readonly long s_serialVersionUID = 1L;
+
+        private int _index = -1;
+
+        public LocaleSyntaxException(string msg):this(msg,0)
+        {     
+        }
+
+        public LocaleSyntaxException(string msg,int errorIndex):base(msg)
+        {
+            _index = errorIndex;
+        }
+
+        public int GetErrorIndex()
+        {
+            return _index;
+        }
     }
 }
